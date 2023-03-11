6cbb402e36f8c09097ab4e0b1c58f707249468c8
Muhammed Irshad <muhammed_irshad@geojit.com>
2023-03-11 14:23:15 +0530

    addnotescreen update

--- lib/screens/add_note.dart
+++ lib/screens/add_note.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -9,8 +10,32 @@ class Addnote extends StatefulWidget {
 }
 
 class _AddnoteState extends State<Addnote> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController bodycontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add Note"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Title",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            TextField(controller: bodycontroller),
+            SizedBox(
+              height: 10,
+            ),
+            Text(" Body",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
+    );
   }
 }
