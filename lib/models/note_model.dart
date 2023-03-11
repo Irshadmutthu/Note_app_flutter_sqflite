161f88e593d11e7a7cdd413f6ee2e16f2fa26307
Muhammed Irshad <muhammed_irshad@geojit.com>
2023-03-11 14:52:52 +0530

    add note update

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,15 +1,20 @@
 class NotesModel {
-  int id;
+  int? id;
   String title;
   String body;
   DateTime creationdate;
   NotesModel(
-      {required this.id,
+      {this.id,
       required this.title,
       required this.body,
       required this.creationdate});
 
   Map<String, dynamic> toMap() {
-    return {"id": id, "title": title, "body": body, "creationdate": creationdate};
+    return {
+      "id": id,
+      "title": title,
+      "body": body,
+      "creationdate": creationdate
+    };
   }
 }
--- lib/screens/add_note.dart
+++ lib/screens/add_note.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:note_app/db/database_provider.dart';
+import 'package:note_app/models/note_model.dart';
 
 class Addnote extends StatefulWidget {
   const Addnote({super.key});
@@ -8,8 +12,20 @@ class Addnote extends StatefulWidget {
 }
 
 class _AddnoteState extends State<Addnote> {
+  addnote(NotesModel notesModel) {
+    try {
+      DatabaseProvider.db.addNote(notesModel);
+      log("note added success");
+    } catch (error) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(error.toString())));
+    }
+  }
+
   TextEditingController titleController = TextEditingController();
   TextEditingController bodycontroller = TextEditingController();
+  String title = '';
+  String body = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,8 +59,28 @@ class _AddnoteState extends State<Addnote> {
               height: 10,
             ),
             Center(
-                child:
-                    ElevatedButton(onPressed: () {}, child: Text("Add note")))
+                child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        title = titleController.text;
+                        body = bodycontroller.text;
+                      });
+                      if (titleController.text.isNotEmpty &&
+                          bodycontroller.text.isNotEmpty) {
+                        addnote(NotesModel(
+                            title: title,
+                            body: body,
+                            creationdate: DateTime.now()));
+                        Navigator.pop(context);
+                        Navigator.pushNamed(context, "home");
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content:
+                                    Text("Enter title and body to continue")));
+                      }
+                    },
+                    child: const Text("Add note")))
           ],
         ),
       ),
